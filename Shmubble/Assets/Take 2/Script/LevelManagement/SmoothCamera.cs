7227197eb33eb8a1a8eb70e10e539b2ad2012e72
Scalexx <nneleman@gmail.com>
2019-02-16 12:51:21 +0100

    Edited camera options
    Added a bounds option for the camera so it can't go beyond a certain
    point. Added tooltips to the existing variables of the camera script.

--- Shmubble/Assets/Take 2/Script/LevelManagement/SmoothCamera.cs
+++ Shmubble/Assets/Take 2/Script/LevelManagement/SmoothCamera.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class SmoothCamera : MonoBehaviour {
 
+    [Header("Target")]
+    [Tooltip("Target to follow.")]
     public Transform lookAt;
 
-    private bool smooth = true;
+    [Header("Movement options")]
+    [Tooltip("Makes camera movement smooth.")]
+    public bool smooth = true;
+    [Tooltip("Makes camera look at target.")]
     public bool cameraPanning;
-    [Range (0,1)]
+    [Tooltip("How fast the camera reaches its target position.")]
+    [Range(0, 1)]
     public float smoothSpeed = 0.125f;
+
+    [Header("Movement restrictions")]
+    [Tooltip("Gives camera bounds.")]
+    public bool bounds;
+    [Tooltip("Stops the camera panning effect when the camera reaches the bounds of the screen.")]
+    public bool stopPanningWhenBound;
+    [Tooltip("Minimum x, y, and z values for the camera. (Check for offset)")]
+    public Vector3 minBounds;
+    [Tooltip("Maximum x, y, and z values for the camera. (Check for offset)")]
+    public Vector3 maxBounds;
+
+    [Space(20)]
+    [Tooltip("Offset position.")]
     public Vector3 offset;
 
     void FixedUpdate()
@@ -27,6 +46,25 @@ public class SmoothCamera : MonoBehaviour {
         if (cameraPanning)
         {
             transform.LookAt(lookAt);
+        }
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x), Mathf.Clamp(transform.position.y, minBounds.y, maxBounds.y), Mathf.Clamp(transform.position.z, minBounds.z, maxBounds.z));
+            if (stopPanningWhenBound)
+            {
+                if (transform.position.x == minBounds.x || transform.position.x == maxBounds.x)
+                {
+                    cameraPanning = false;
+                }
+                else
+                {
+                    cameraPanning = true;
+                }
+            }
+            else
+            {
+                cameraPanning = true;
+            }
         }
     }
 }
