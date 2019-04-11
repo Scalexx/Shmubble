499ffe06e8677af76017812c34ef5eec5733ad92
Scalexx <nneleman@gmail.com>
2019-04-11 14:24:27 +0200

    Added boulder functionality
    Added functionality to the boulder

---
+++ Shmubble/Assets/RotationScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    public float randX;
+    public float randY;
+    public float randZ;
+
+    void OnEnable()
+    {
+        this.transform.localRotation = Quaternion.Euler (Random.Range(0, randX), Random.Range(0, randY), Random.Range(0, randZ));
+    }
+
+}
